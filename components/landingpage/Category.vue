79f591fdfaebbd51c072afe79d2405a5bf5e9778
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2025-01-16 05:44:57 +0700

    fix: bugs category

--- components/landingpage/Category.vue
+++ components/landingpage/Category.vue
@@ -16,11 +16,11 @@ import { Navigation } from "swiper/modules";
             :slides-per-view="5"
             :space-between="10"
             :loop="true"
-            class="mySwiper mt-8"
+            class="mySwiper mt-8 h-[197px]"
          >
             <SwiperSlide>
                <div
-                  class="py-[30px] hover:pb-[48px] group rounded-lg transition-all duration-300 bg-cupcake"
+                  class="py-[30px] hover:pb-[48px] group rounded-lg overflow-hidden relative transition-all duration-300 bg-cupcake"
                >
                   <div class="inset-0 absolute">
                      <div
@@ -40,7 +40,7 @@ import { Navigation } from "swiper/modules";
             </SwiperSlide>
             <SwiperSlide>
                <div
-                  class="py-[30px] hover:pb-[48px] group rounded-lg transition-all duration-300 bg-pizza"
+                  class="py-[30px] hover:pb-[48px] group rounded-lg overflow-hidden relative transition-all duration-300 bg-pizza"
                >
                   <div class="inset-0 absolute">
                      <div
@@ -60,7 +60,7 @@ import { Navigation } from "swiper/modules";
             </SwiperSlide>
             <SwiperSlide>
                <div
-                  class="py-[30px] hover:pb-[48px] group rounded-lg transition-all duration-300 bg-kebab"
+                  class="py-[30px] hover:pb-[48px] group rounded-lg overflow-hidden relative transition-all duration-300 bg-kebab"
                >
                   <div class="inset-0 absolute">
                      <div
@@ -80,7 +80,7 @@ import { Navigation } from "swiper/modules";
             </SwiperSlide>
             <SwiperSlide>
                <div
-                  class="py-[30px] hover:pb-[48px] group rounded-lg transition-all duration-300 bg-salmon"
+                  class="py-[30px] hover:pb-[48px] group rounded-lg overflow-hidden relative transition-all duration-300 bg-salmon"
                >
                   <div class="inset-0 absolute">
                      <div
@@ -100,7 +100,107 @@ import { Navigation } from "swiper/modules";
             </SwiperSlide>
             <SwiperSlide>
                <div
-                  class="py-[30px] hover:pb-[48px] group rounded-lg transition-all duration-300 bg-doughnut"
+                  class="py-[30px] hover:pb-[48px] group rounded-lg overflow-hidden relative transition-all duration-300 bg-doughnut"
+               >
+                  <div class="inset-0 absolute">
+                     <div
+                        class="bg-[url('~/assets/images/bg-category.png')] h-full bg-cover bg-center opacity-0 group-hover:opacity-[7%] transition-all duration-300"
+                     ></div>
+                  </div>
+                  <div class="flex items-center flex-col relative z-10">
+                     <img
+                        class="w-[47px] h-[47px]"
+                        src="~/assets/icons/ic_doughnut.png"
+                        alt="icon category"
+                     />
+                     <h5 class="font-medium capitalize mt-6">doughnut</h5>
+                     <h6 class="text-sm mt-1">22 Items</h6>
+                  </div>
+               </div>
+            </SwiperSlide>
+            <SwiperSlide>
+               <div
+                  class="py-[30px] hover:pb-[48px] group rounded-lg overflow-hidden relative transition-all duration-300 bg-cupcake"
+               >
+                  <div class="inset-0 absolute">
+                     <div
+                        class="bg-[url('~/assets/images/bg-category.png')] h-full bg-cover bg-center opacity-0 group-hover:opacity-[7%] transition-all duration-300"
+                     ></div>
+                  </div>
+                  <div class="flex items-center flex-col relative z-10">
+                     <img
+                        class="w-[47px] h-[47px]"
+                        src="~/assets/icons/ic_cupcake.png"
+                        alt="icon category"
+                     />
+                     <h5 class="font-medium capitalize mt-6">cupcake</h5>
+                     <h6 class="text-sm mt-1">22 Items</h6>
+                  </div>
+               </div>
+            </SwiperSlide>
+            <SwiperSlide>
+               <div
+                  class="py-[30px] hover:pb-[48px] group rounded-lg overflow-hidden relative transition-all duration-300 bg-pizza"
+               >
+                  <div class="inset-0 absolute">
+                     <div
+                        class="bg-[url('~/assets/images/bg-category.png')] h-full bg-cover bg-center opacity-0 group-hover:opacity-[7%] transition-all duration-300"
+                     ></div>
+                  </div>
+                  <div class="flex items-center flex-col relative z-10">
+                     <img
+                        class="w-[47px] h-[47px]"
+                        src="~/assets/icons/ic_pizza.png"
+                        alt="icon category"
+                     />
+                     <h5 class="font-medium capitalize mt-6">pizza</h5>
+                     <h6 class="text-sm mt-1">25 Items</h6>
+                  </div>
+               </div>
+            </SwiperSlide>
+            <SwiperSlide>
+               <div
+                  class="py-[30px] hover:pb-[48px] group rounded-lg overflow-hidden relative transition-all duration-300 bg-kebab"
+               >
+                  <div class="inset-0 absolute">
+                     <div
+                        class="bg-[url('~/assets/images/bg-category.png')] h-full bg-cover bg-center opacity-0 group-hover:opacity-[7%] transition-all duration-300"
+                     ></div>
+                  </div>
+                  <div class="flex items-center flex-col relative z-10">
+                     <img
+                        class="w-[47px] h-[47px]"
+                        src="~/assets/icons/ic_kebab.png"
+                        alt="icon category"
+                     />
+                     <h5 class="font-medium capitalize mt-6">kebab</h5>
+                     <h6 class="text-sm mt-1">12 Items</h6>
+                  </div>
+               </div>
+            </SwiperSlide>
+            <SwiperSlide>
+               <div
+                  class="py-[30px] hover:pb-[48px] group rounded-lg overflow-hidden relative transition-all duration-300 bg-salmon"
+               >
+                  <div class="inset-0 absolute">
+                     <div
+                        class="bg-[url('~/assets/images/bg-category.png')] h-full bg-cover bg-center opacity-0 group-hover:opacity-[7%] transition-all duration-300"
+                     ></div>
+                  </div>
+                  <div class="flex items-center flex-col relative z-10">
+                     <img
+                        class="w-[47px] h-[47px]"
+                        src="~/assets/icons/ic_salmon.png"
+                        alt="icon category"
+                     />
+                     <h5 class="font-medium capitalize mt-6">salmon</h5>
+                     <h6 class="text-sm mt-1">22 Items</h6>
+                  </div>
+               </div>
+            </SwiperSlide>
+            <SwiperSlide>
+               <div
+                  class="py-[30px] hover:pb-[48px] group rounded-lg overflow-hidden relative transition-all duration-300 bg-doughnut"
                >
                   <div class="inset-0 absolute">
                      <div
